b0395d49e1b4856f92cb14cc48c35e96139faa37
nikos <nikosglikis@gmail.com>
2016-02-28 12:40:15 +0200

    Detects bad images

--- src/main/java/com/scanners/ccsnaptv/Main.java
+++ src/main/java/com/scanners/ccsnaptv/Main.java
@@ -1,6 +1,12 @@
 package com.scanners.ccsnaptv;
 
+import com.toortools.tor.TorHelper;
+
 public class Main
 {
-
+    public static void main(String args[])
+    {
+        TorHelper.torifySimple(true);
+        new CcSnapTv();
+    }
 }
